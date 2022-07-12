24481d992f1638e02311f41cfcec67211a521c1f
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-07-11 21:11:30 -0300

    Refactoring last components to typescript

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,30 +1,29 @@
-import { Component, createRef } from 'react';
+import {createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FoodForm } from '../../types';
+import { FormHandles, SubmitHandler } from '@unform/core';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface ModalAddFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food: FoodForm) => void;
+}
 
-    this.formRef = createRef();
-  }
+const ModalAddFood = ({isOpen,setIsOpen,handleAddFood}: ModalAddFoodProps) => {
+  const formRef = createRef<FormHandles>();
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
-
+  const handleSubmit: SubmitHandler<FoodForm> = async (data) => {
     handleAddFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen } = this.props;
-
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -41,7 +40,6 @@ class ModalAddFood extends Component {
         </Form>
       </Modal>
     );
-  }
 };
 
 export default ModalAddFood;
